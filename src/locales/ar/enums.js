ed6fecda774bb30bc7117c8e190d6c3829873ec2
masoud <masoudmotahari1380@gmail.com>
2024-05-28 00:37:12 +0430

    add arabic language

---
+++ src/locales/ar/enums.js
@@ -0,0 +1,11 @@
+import ComponentSize from '@/enums/ComponentSize';
+
+export default {
+    "component_size": {
+        [ComponentSize.XS]: 'صغير جداً',
+        [ComponentSize.SM]: 'صغير',
+        [ComponentSize.MD]: 'متوسط',
+        [ComponentSize.LG]: 'كبير',
+        [ComponentSize.XL]: 'كبير جداً'
+    }
+};
---
+++ src/locales/ar/index.js
@@ -0,0 +1,6 @@
+import Messages from '@/locales/ar/messages';
+import Enums from '@/locales/ar/enums';
+
+export default Object.assign(Messages, {
+    enums: Enums
+});
