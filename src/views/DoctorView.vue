532b95eeec4317e9dd5c246d6c9d845bc63c030d
masoud <masoudmotahari1380@gmail.com>
2024-05-28 00:37:36 +0430

    add doctor page view

---
+++ src/views/DoctorView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row g-3">
+            <div class="col-9 ">
+                <p class="bg-primary">doctor</p>
+            </div>
+            <div class="col-3 ">
+                <p class="bg-danger">times</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    setup() {
+        return {
+
+        }
+    }
+}
+</script>
