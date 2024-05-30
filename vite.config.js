ee69e25e367793ed0772767d5be4ac90dc633853
masoud <masoudmotahari1380@gmail.com>
2024-05-30 17:04:23 +0330

    change rtlcss plugin use case

--- vite.config.js
+++ vite.config.js
@@ -4,28 +4,35 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import rtlcss from 'rtlcss';
 
-function rtlcssSpecificFile() {
-    return {
-        name: 'RtlcssSpecificFile',
-
-        transform(src, id) {
-            // #TODO Can be handle better
-            if (id.endsWith('/assets/scss/app-rtl.scss')) {
-                return {
-                    code: rtlcss.process(src)
-                };
-            }
-        }
-    }
-}
+// function rtlcssSpecificFile() {
+//     return {
+//         name: 'RtlcssSpecificFile',
+//
+//         transform(src, id) {
+//             // #TODO Can be handle better
+//             if (id.endsWith('/assets/scss/app-rtl.scss')) {
+//                 return {
+//                     code: rtlcss.process(src)
+//                 };
+//             }
+//         }
+//     }
+// }
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), rtlcssSpecificFile()],
+    plugins: [vue()],
 
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
-    }
+    },
+
+    // Todo => set rtl by rtlcssSpecificFile plugin
+    css: {
+        postcss: {
+            plugins: [rtlcss()]
+        }
+    },
 });
