d33cd8ff7e54d3452ad6c141d428851b049d8639
masoud <masoudmotahari1380@gmail.com>
2024-05-30 18:33:05 +0330

    show first time available

--- src/components/Doctor/DoctorAppointment.vue
+++ src/components/Doctor/DoctorAppointment.vue
@@ -1,10 +1,13 @@
 <template>
-    <div class="bg-white p-4 rounded">
+    <div class="bg-white p-5 rounded d-flex flex-column gap-7">
         <div v-for="item of items" :key="item.id">
-            <div>{{item.name}}</div>
-            <div>
-                <button class="btn btn-primary" type="button">
-                    دریافت نوبت
+            <p class="fs-lg-3 fs-2 fw-medium mb-1">{{ item.name }}</p>
+            <div class="d-flex align-items-center justify-content-between">
+                <p class="fs-lg-3 fs-2">
+                    {{ $t('first time') }} {{ getFistTimeText(item.first_time) }}
+                </p>
+                <button class="btn btn-primary px-lg-4 py-lg-2  fs-lg-3 fs-2 fw-bold" type="button">
+                    {{ $t('get turn') }}
                 </button>
             </div>
         </div>
@@ -12,6 +15,8 @@
 </template>
 
 <script>
+    import DateTime from '@/utils/date-time';
+
     export default {
         name: "DoctorAppointment",
 
@@ -19,18 +24,23 @@
             const items = [
                 {
                     name: 'نوبت مطب',
-                    first_time: "30-5-2024",
+                    first_time: "30-05-2024",
                     id: 1
                 },
                 {
                     name: 'نوبت نوار مغز',
-                    first_time: "4-6-2024",
+                    first_time: "4-06-2024",
                     id: 1
                 }
             ];
 
+            function getFistTimeText(firsTime) {
+                return DateTime.make().gregorian().parse('dd-MM-yyyy', firsTime).jalali().format('dddd dd MMMM');
+            }
+
             return {
-                items
+                items,
+                getFistTimeText
             };
         }
 
