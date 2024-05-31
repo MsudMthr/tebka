cf9ca72b1f80cac8fbae2f7ea18a01caeba61745
masoud <masoudmotahari1380@gmail.com>
2024-05-31 18:45:27 +0330

    fix responsive style

--- src/components/Doctor/DoctorProfile.vue
+++ src/components/Doctor/DoctorProfile.vue
@@ -2,15 +2,15 @@
     <div class="doctor-details__profile d-flex align-items-center bg-white rounded p-4">
         <img :alt="doctor.name" :src="doctor.image" class="doctor-details__profile-image">
 
-        <div class="ms-5 d-flex flex-column gap-3 flex-fill">
-            <div class="d-flex justify-content-between">
-                <h1 class="doctor-details__profile-name mb-0">{{ doctor.name }}</h1>
-                <div class="align-self-start d-flex gap-1">
-                    <p class="fs-3">{{doctor.rate}}</p>
+        <div class="ms-4 d-flex flex-column flex-fill">
+            <div class="d-flex justify-content-between mb-1 mb-lg-4">
+                <h1 class="doctor-details__profile-name mb-0 me-4">{{ doctor.name }}</h1>
+                <div class="align-self-start d-flex">
+                    <p class="fs-3 me-1">{{doctor.rate}}</p>
                     <i class="bi-star-fill text-warning"></i>
                 </div>
             </div>
-            <h4 class="fs-3 text-gray mb-0">{{ doctor.expertise }}</h4>
+            <h4 class="doctor-details__profile-expertise text-gray mb-0">{{ doctor.expertise }}</h4>
         </div>
 
     </div>
