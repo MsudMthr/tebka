4cfb2c303a9f08e9402429190ec4693744e224c9
masoud <masoudmotahari1380@gmail.com>
2024-05-31 19:02:28 +0330

    feat(DoctorView): get doctor from controller and pass to components

--- src/views/DoctorView.vue
+++ src/views/DoctorView.vue
@@ -1,33 +1,45 @@
 <template>
-    <VLayout>
-        <div class="doctor-details container mt-2 mt-md-4">
-            <div class="row g-5">
-                <div class="col-12 col-lg-8">
-                    <DoctorProfile />
-                    <div class="d-block d-lg-none mt-4">
-                        <DoctorAppointment />
-                    </div>
+<VLayout>
+    <div class="doctor-details container mt-2 mt-md-4">
+        <div class="row g-5">
+
+            <div class="col-12 col-lg-8">
+                <DoctorProfile :doctor="doctor"/>
+
+                <div class="d-block d-lg-none mt-4">
+                    <DoctorAppointment/>
                 </div>
-                <div class="col-12 col-md-5 col-lg-4 d-none d-lg-block">
-                    <DoctorAppointment />
-                </div>
+
+                <DoctorDescription :doctor="doctor"/>
+            </div>
+
+            <div class="col-12 col-md-5 col-lg-4 d-none d-lg-block">
+                <DoctorAppointment/>
             </div>
         </div>
-    </VLayout>
+    </div>
+</VLayout>
 </template>
 
 <script>
-
+// Components
 import DoctorAppointment from '@/components/Doctor/DoctorAppointment.vue';
 import VLayout from '@/components/layout/VLayout.vue';
 import DoctorProfile from '@/components/Doctor/DoctorProfile.vue';
+import DoctorDescription from '@/components/Doctor/DoctorDescription.vue';
+
+// Controller
+import useDoctor from '@/controller/DoctorController';
 
 export default {
-    components: { DoctorProfile, VLayout, DoctorAppointment },
-    setup() {
-        return {
+    components: {DoctorDescription, DoctorProfile, VLayout, DoctorAppointment},
 
-        }
+    setup() {
+        const {doctor} = useDoctor();
+
+        return {
+            doctor
+        };
     }
-}
+};
 </script>
