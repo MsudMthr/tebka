b3bc4de6a70070a29671c3a1b5c3e71a2550ab81
masoud <masoudmotahari1380@gmail.com>
2024-06-02 21:00:37 +0330

    add contact info

--- src/controller/DoctorController.js
+++ src/controller/DoctorController.js
@@ -5,7 +5,10 @@ const useDoctor = () => {
         name: 'علی اکبر سلطانیان',
         expertise: 'متخصص مغز و اعصاب و ستون فقرات',
         rate: '4.6',
-        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj0MoQrYbawqdVlieVCav3xX7c44OMf43K2A&s'
+        image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj0MoQrYbawqdVlieVCav3xX7c44OMf43K2A&s',
+        phone: "۰۵۱ - ۳۸۴۴۵۶۷۸",
+        mobile: '۰۹۰۲۵۵۵۵۵۶۷۸',
+        address: 'مشهد، میدان بیمارستان امام رضا، ابتدای خیابان رازی، ساختمان پزشکان رازی (پارسیان)، طبقه 3، واحد ۳۰'
     })
 
     return {
